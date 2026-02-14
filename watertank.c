db70b9bb398c2111219de7a3f7c9ac1795325433
Akanksha <akanksharesearch5@gmail.com>
2026-02-14 22:41:42 +0530

    Update watertank.c

--- watertank.c
+++ watertank.c
@@ -1,30 +1,35 @@
 #include<stdio.h>
 int main()
 {
+    int a[50];
 	int area=0;
 	int amount=0;
-	int n1,n2;
+	int n1,n2,size;
+	printf("enter the size:");
+	scanf("%d",&size);
+	printf("enter the element:\n");
+	for(int i=0;i<size;i++)
+	scanf("%d",&a[i]);
 	for(int i=0;i<=(size-1);i++)
 	{
 		for(int j=0;j<=(size-1);j++)
 		{
 			if(a[i]>a[j])
 			{
-				amount=a[i]*(j-1);
+				amount=a[i]*a[j];
 			}
 			else
 			{
-				amount=a[j]*(j-1);
+				amount=a[j]*a[i];
 			}
 			if(amount>area)
+			{
 			n1=a[i];
 			n2=a[j];
 			area=amount;
+			}
 		}
 	}
-}
-{
-printf("n1=%d\tn2=%d\tarea=%d \n",n1,n2,area);
-}
+ printf("n1=%d\tn2=%d\tarea=%d \n",n1,n2,area);
   return 0;
 }
